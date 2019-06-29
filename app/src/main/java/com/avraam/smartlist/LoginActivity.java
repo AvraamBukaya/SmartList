a0f06290ce8b70188309865841dfded723035fe6
Avraam_Bukaya <ablife91@gmail.com>
2019-06-29 16:50:02 +0300

    Initial commit

---
+++ app/src/main/java/com/avraam/smartlist/LoginActivity.java
@@ -0,0 +1,130 @@
+package com.avraam.smartlist;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.signin.internal.SignInHubActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final int MY_REQUEST_CODE = 1213 ;
+    private List<AuthUI.IdpConfig>providers;
+    private Button btn_sign_out;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Init providers
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build()
+        );
+        showsSignInOptions();
+        btn_sign_out = findViewById(R.id.sign_out_btn);
+        onClickSignOutBtn();
+
+    }
+
+    private void showsSignInOptions()
+    {
+       startActivityForResult(
+               AuthUI.getInstance().createSignInIntentBuilder().
+                       setAvailableProviders(providers).
+                       setTheme(R.style.LoginTheme).
+                       build(),MY_REQUEST_CODE
+
+       );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == MY_REQUEST_CODE)
+        {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if(requestCode == RESULT_OK)
+            {
+                //Get User
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Intent MainScreenSender = new Intent(this,MainActivity.class);
+
+                finish();
+
+                //Show Email on Toast
+                popMessage(user.getEmail());
+                //Toast.makeText(this, ""+user.getEmail(), Toast.LENGTH_SHORT).show();
+
+                //Set Button Sign Out
+                btn_sign_out.setEnabled(true);
+            }
+            else if(requestCode == RESULT_CANCELED){
+                popMessage("Sign-In cancelled");
+            }
+            else {
+                popMessage("Login process failed for an unknown reason");
+            }
+        }
+    }
+
+    public void onClickSignOutBtn()
+    {
+        btn_sign_out.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                AuthUI.getInstance()
+                        .signOut(LoginActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>()
+                        {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task)
+                            {
+                                btn_sign_out.setEnabled(false);
+                                showsSignInOptions();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e)
+                    {
+                        popMessage(e.getMessage());
+                    }
+                });
+            }
+        });
+    }
+
+    public void popMessage(String message){
+        Toast.makeText(this, ""+message, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/avraam/smartlist/MainActivity.java
@@ -0,0 +1,14 @@
+package com.avraam.smartlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
