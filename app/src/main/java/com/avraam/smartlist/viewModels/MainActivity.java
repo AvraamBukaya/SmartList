814a5c866ac985e3e0f58636c7115e24ff458d81
Avraam_Bukaya <ablife91@gmail.com>
2019-07-08 19:56:50 +0300

    build dashboard finish

--- app/src/main/java/com/avraam/smartlist/viewModels/MainActivity.java
+++ app/src/main/java/com/avraam/smartlist/viewModels/MainActivity.java
@@ -1,12 +1,12 @@
 package com.avraam.smartlist.viewModels;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-
 import com.avraam.smartlist.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     /*    add_product_btn = findViewById(R.id.add_productBtn);
         add_product_btn.setOnClickListener(new View.OnClickListener() {
             @Override
