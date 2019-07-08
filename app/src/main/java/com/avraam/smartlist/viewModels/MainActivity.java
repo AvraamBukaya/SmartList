a9fa7ef50b7bde34f2ad91ca080bf79ac9d08336
Avraam_Bukaya <ablife91@gmail.com>
2019-07-08 23:22:12 +0300

    build dashboard finish

--- app/src/main/java/com/avraam/smartlist/viewModels/MainActivity.java
+++ app/src/main/java/com/avraam/smartlist/viewModels/MainActivity.java
@@ -3,31 +3,43 @@ package com.avraam.smartlist.viewModels;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 import com.avraam.smartlist.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button add_product_btn;
+    private ImageButton add_btn;
+    private ImageButton view_list_btn;
+    private ImageButton attach_file_btn;
+    private ImageButton report_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add_btn = findViewById(R.id.image_btn_add_product);
+        view_list_btn = findViewById(R.id.image_btn_view_list);
+        attach_file_btn = findViewById(R.id.image_btn_attach_file);
+        report_btn = findViewById(R.id.image_btn_report);
+        onClickAtAddBtnListener();
+    }
 
 
-    /*    add_product_btn = findViewById(R.id.add_productBtn);
-        add_product_btn.setOnClickListener(new View.OnClickListener() {
+    public void onClickAtAddBtnListener(){
+        add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent addProductScreen = new Intent(MainActivity.this,AddProduct.class);
-                startActivity(addProductScreen);
+                Intent moveToAddProductScreen = new Intent(MainActivity.this,AddProduct.class);
+                startActivity(moveToAddProductScreen);
                 finish();
-
             }
-        });*/
+        });
     }
 }
